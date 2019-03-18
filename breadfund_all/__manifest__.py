e247de19d8618aa0d2b0fb15e5e830bb1b83dbad
Tom Blauwendraat <antiflu@gmail.com>
2019-03-18 11:13:26 +0100

    [ADD] breadfund_all module

---
+++ breadfund_all/__manifest__.py
@@ -0,0 +1,13 @@
+﻿{
+    'name': 'All breadfund functionality',
+    'version': '10.0.1.0.0',
+    'summary': 'Installs all modules for breadfund functionality',
+    'author': 'Sunflower IT',
+    'website': 'http://www.sunflowerweb.nl',
+    'category': 'Extra Tools',
+    'images': [],
+    'depends': ['account'],
+    'data': [
+    ],
+    'installable': True,
+}
